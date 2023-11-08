3a4a43e83aae415c15d5b9ec146598471b7e1c00
TaraskiN85 <taras12304@i.ua>
2023-11-08 22:41:36 +0200

    fix export from helper.js

--- src/helper.js
+++ src/helper.js
@@ -1,9 +1,11 @@
 import { Notify } from 'notiflix';
 
-export default showError = message => {
+const showError = message => {
   Notify.failure(`${message}`, {
     timeout: 1500,
     position: 'center-top',
     distance: '100px',
   });
 };
+
+export default showError;
